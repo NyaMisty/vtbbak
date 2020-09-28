1620ccb46e447c2a3d93fa5d7a9327ece8abd391
Misty <gyc990326@gmail.com>
2020-09-29 01:46:27 +0800

    improve config load

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ CONFIG = {
     "redis": "redis://172.23.115.120",
     "amqp": 'pyamqp://guest:guest@172.23.115.120:5672'
 }
-def load_config(path):
+def load_config():
+    path = os.path.join(os.path.dirname(__file__), "config.json")
     try:
         with open(path, 'r') as f:
             CONFIG.update(json.load(f))
--- demo_app/__init__.py
+++ demo_app/__init__.py
@@ -1,4 +1,3 @@
 import os
 from config import load_config
-CONFIG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json")
-load_config(CONFIG_PATH)
+load_config()
